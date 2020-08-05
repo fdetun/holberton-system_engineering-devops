9857e633e0897e119b948ef7a330dbf4506bb5ae
fdetun <fouedads@gmail.com>
2020-08-05 17:47:19 +0100

    4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 #task 4
 exec { 'task4':
-  command  => 'echo -e "\tPasswordAuthentication no \n tIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+  command  => 'echo -e "\tPasswordAuthentication no \n \tIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
   provider => 'shell',
 }
