1276e5059de3c9a481918e36a78d7c94308c4b09
fdetun <fouedads@gmail.com>
2020-09-14 18:44:44 +0100

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""gathering from json and exprong ot to csv"""
+import json
+import requests
+import sys
+
+
+def getfunc(url):
+    """ function to rturn json format from url"""
+    r = requests.get(url)
+    f = r.json()
+    return f
+
+
+if __name__ == "__main__":
+    num = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(num)
+    url2 = "https://jsonplaceholder.typicode.com/users?id={}".format(num)
+    uname = getfunc(url2)[0]["username"]
+    arr = []
+    mydict = {"task": "", "completed": False, "username": uname}
+    for k, v in getfunc(url)[0].items():
+        if k == "title":
+            mydict["task"] = v
+        else:
+            mydict[k] = v
+        arr.append(mydict)
+    my = {num: arr}
+    with open(str(num) + ".json", 'w') as f:
+        json.dump(my, f)
