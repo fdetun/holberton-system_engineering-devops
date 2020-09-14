d965493b4012ef0469fb9306a8068229217f5132
fdetun <fouedads@gmail.com>
2020-09-14 16:15:30 +0100

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""gathering from json"""
+import requests
+import sys
+
+
+def getfunc(url):
+    """ function to rturn json format from url"""
+    r = requests.get(url)
+    f = r.json()
+    return f
+
+
+num = int(sys.argv[1])
+arr = []
+fal = 0
+url = "https://jsonplaceholder.typicode.com/todos"
+url2 = "https://jsonplaceholder.typicode.com/users"
+f = getfunc(url)
+for i in f:
+    if i["userId"] == num:
+        if i["completed"]:
+            arr.append(i["title"])
+        else:
+            fal += 1
+f2 = getfunc(url2)
+for j in f2:
+    if j["id"] == num:
+        nam = j["name"]
+print("Employee {} is done with tasks({}/{}):".format(nam, len(arr), len(arr) + fal))
+for n in arr:
+    print("\t{} ".format(n))
