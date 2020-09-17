1478c79d2dd46711f20c717e8517c93a8f4e5247
fdetun <fouedads@gmail.com>
2020-09-17 16:14:12 +0100

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """function return the number of subscribers of subreddit"""
+    useragent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"
+    url = "https://www.reddit.com/r/{}/top/.json?limit=10".format(subreddit)
+    r = requests.get(url, headers={'User-agent': useragent})
+    try:
+        f = r.json()["data"]['children']
+        for i in f:
+            print(i["data"]['title'])
+    except:
+        return 0
