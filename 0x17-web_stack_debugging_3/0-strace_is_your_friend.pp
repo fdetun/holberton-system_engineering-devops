12481ece989d81cc11d81e9b1313ac75cb605aeb
fdetun <fouedads@gmail.com>
2020-09-30 22:18:59 +0100

    ok

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,8 +4,3 @@ exec {'fix wordpress setting file':
     path    => '/bin',
     command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
 }
-exec {'restart apache2':
-    user    => root,
-    path    => '/bin',
-    command => 'sudo service apache2 restart',
-}
