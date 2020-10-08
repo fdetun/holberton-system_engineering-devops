7cc53b47fa995a80b8a5998141181474caa483c2
fdetun <fouedads@gmail.com>
2020-10-08 22:49:13 +0100

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+#0x1B-web_stack_debugging_4 
+exec {'debugging':
+    user    => root,
+    path    => '/bin',
+    command => 'sudo sed -i 's/ULIMIT.*/ULIMIT="-n 10000"/' /etc/default/nginx && service nginx restart',
+}
