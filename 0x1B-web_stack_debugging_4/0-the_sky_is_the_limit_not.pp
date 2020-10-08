17b2b7ffa838aceabd5ea0757cc9d6026125ebbc
fdetun <fouedads@gmail.com>
2020-10-08 23:27:50 +0100

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,10 @@
 #0x1B-web_stack_debugging_4 
 exec {'debugging':
-    user    => root,
     path    => '/bin/',
-    command => 'sudo sed -i "s/-n 10000/-n 4096/g" /etc/default/nginx',
+    command => 'sudo sed -i "s/-n 15/-n 4096/g" /etc/default/nginx',
 }
 
 -> exec { 'nginx':
-    user     => root,
     provider => 'shell',
     command  => 'sudo service nginx restart',
 }
